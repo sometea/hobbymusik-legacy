3939e1230543e062268385c46d8679e95d6a7f5a
David Dasenbrook <david.dasenbrook@gmail.com>
2018-05-06 18:06:52 +0200

    add basic components and copy-paste templates from keystone version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaticpageComponent } from './staticpage/staticpage.component';
+import { PostsComponent } from './posts/posts.component';
+import { ReleasesComponent } from './releases/releases.component';
+import { ReleaseComponent } from './release/release.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaticpageComponent,
+    PostsComponent,
+    ReleasesComponent,
+    ReleaseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/staticpage/staticpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staticpage',
+  templateUrl: './staticpage.component.html',
+  styleUrls: ['./staticpage.component.css']
+})
+export class StaticpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
